a6100867ea0d45902c5185de28a10c75287a5e64
mahmoud-JO <mahmoudtradat20055jo@gmail.com>
2026-03-19 22:32:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function validateForm() {
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let country = document.getElementById("country").value;
+let comments = document.getElementById("comments").value;
+
+if(name === "" || email === "" || country === "" || comments === ""){
+
+alert("Please fill in all fields");
+
+return false;
+
+}
+
+alert("Form submitted successfully!");
+
+return true;
+
+}
